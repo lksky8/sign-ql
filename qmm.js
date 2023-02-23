516968d8d8f35aa0e576ccda7d62ba0e289bbe46
lksky8 <60889526+lksky8@users.noreply.github.com>
2023-02-23 18:03:24 +0800

    Update qmm.js

--- qmm.js
+++ qmm.js
@@ -16,7 +16,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 const md5 = require('md5');
 const {log} = console;
 const Notify = 0; //0为关闭通知，1为打开通知,默认为1
-const debug = 1; //0为关闭调试，1为打开调试,默认为0
+const debug = 0; //0为关闭调试，1为打开调试,默认为0
 //////////////////////
 let qmm = process.env.qmm;
 let qmmArr = [];
